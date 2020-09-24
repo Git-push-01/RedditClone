c62fa6ad249bb799d8dd1ccb88b8f5daa9499259
Hugo Escandon <hescandon6@gmail.com>
2020-09-24 17:43:09 -0400

    reddit scraper title

--- scraper.js
+++ scraper.js
@@ -1,15 +1,17 @@
 const puppeteer = require("puppeteer");
 
-async () => {
-  const browser = await puppeteer.launch({
-    headless: true
-  });
+(async () => {
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.reddit.com/new/", { waitUntil: "networkidle0" });
+  await page.goto("https://www.reddit.com/new/", {waitUntil: 'networkidle2'});
   await page.screenshot({ path: "example.png" });
   await page.waitForSelector("body");
-  const rposts = await page.evaluate(() => Array.from(document.body.querySelectorAll(".Post")));
+  const rposts = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("h3")).map(
+      (title) => title.textContent
+    )
+  );
 
   console.log(rposts);
   await browser.close();
-};
+})();
