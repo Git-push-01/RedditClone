ac8bf222bf0ac40aa8c36b7410f9acf033a63cd9
Hugo Escandon <hescandon6@gmail.com>
2020-09-28 18:51:21 -0400

    puppeteer selector and OnPageClick

--- scraper.js
+++ scraper.js
@@ -1,16 +1,18 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto("https://www.reddit.com/r/politics/new/", {
     waitUntil: "networkidle2",
   });
+  await page.click('a')
+  await page.setViewport({width: 1000, height: 500})
   await page.screenshot({ path: "example.png" });
   await page.waitForSelector("body");
 
   const tags = await page.evaluate(() =>
-    Array.from(document.body.querySelectorAll("a")).map((post) => ({
+    Array.from(document.body.querySelectorAll('a')).map((post) => ({
       title: post.innerText,
       link: post.href,
     }))
