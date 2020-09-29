37ba86f83cd860af7facaca0f41e951dd9f87812
Hugo Escandon <hescandon6@gmail.com>
2020-09-29 10:50:14 -0400

    link sms test

--- scraper.js
+++ scraper.js
@@ -1,14 +1,14 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://www.reddit.com/r/politics/new/", {
     waitUntil: "networkidle2",
   });
-  await page.click('a')
-  await page.setViewport({width: 1000, height: 500})
-  await page.screenshot({ path: "example.png" });
+  // await page.click('a')
+  // await page.setViewport({width: 1000, height: 500})
+  // await page.screenshot({ path: "example.png" });
   await page.waitForSelector("body");
 
   const tags = await page.evaluate(() =>
@@ -18,7 +18,11 @@ const puppeteer = require("puppeteer");
     }))
   );
 
-  console.log(tags);
+   console.log(tags[18].title);
+   console.log(tags[19].link)
+//   tags.forEach(function (value, i) {
+//     console.log('%d: %s', i, value);
+// });
 
   await browser.close();
 })();
--- textSend.js
+++ textSend.js
@@ -5,6 +5,9 @@ const client = require("twilio")(accountSid, authToken);
 client.messages.create({
     to: '+19083800126',
     from: '+12564195619',
-    body: 'love you boo'
+    body:
+     "*** " +
+      ''+ " ***"  +
+      ''
 
   }).then((message) => console.log(message.sid));
