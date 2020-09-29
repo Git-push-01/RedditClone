14c3f2bb0a193a7e005aeb11000336499fc5463c
Hugo Escandon <hescandon6@gmail.com>
2020-09-29 16:37:57 -0400

    next

--- scraper.js
+++ scraper.js
@@ -1,28 +1,36 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   await page.goto("https://www.reddit.com/r/politics/new/", {
     waitUntil: "networkidle2",
+
   });
-  // await page.click('a')
-  // await page.setViewport({width: 1000, height: 500})
-  // await page.screenshot({ path: "example.png" });
+  const newsPost = []
   await page.waitForSelector("body");
 
+
   const tags = await page.evaluate(() =>
-    Array.from(document.body.querySelectorAll('a')).map((post) => ({
+    Array.from(document.body.querySelectorAll("a")).map((post) => ({
       title: post.innerText,
       link: post.href,
-    }))
-  );
 
-   console.log(tags[18].title);
-   console.log(tags[19].link)
-//   tags.forEach(function (value, i) {
-//     console.log('%d: %s', i, value);
-// });
+    }))
+  )
+
+  
+  const newTitle = tags[18].title
+
+
+  const newLink =   tags[19].link;
+
+
+
+
+  console.log(newTitle);
+  console.log(newLink);
+
 
   await browser.close();
 })();
--- textSend.js
+++ textSend.js
@@ -1,13 +1,19 @@
+const newTitle = require("./scraper");
 
-const { accountSid, authToken} = require('./config');
+const newLink = require("./scraper");
+
+const { accountSid, authToken } = require("./config");
 const client = require("twilio")(accountSid, authToken);
 
-client.messages.create({
-    to: '+19083800126',
-    from: '+12564195619',
-    body:
-     "*** " +
-      ''+ " ***"  +
-      ''
 
-  }).then((message) => console.log(message.sid));
+const topRedditNews = async(newTitle, newLink) => {
+  if(newTitle) {
+    await client.messages
+      .create({
+        to: "+19083804770",
+        from: "+12564195619",
+        body: "*** " + newTitle + " ***" + newLink,
+      })
+      .then((message) => console.log(message.sid));
+  }
+};
