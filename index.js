856b9245b37c5f79890760da51147512364db0c6
Hugo Escandon <hescandon6@gmail.com>
2020-10-08 19:11:54 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
  // const module1 = require("./getPosts")
+const schedule = require("node-schedule");
 const module2 = require("./postResults")
 const module3 = require("./textSend")
 // module1()
 module2()
+const schechJob = schedule.scheduleJob("* * * * *", function () {
 module3()
+});
