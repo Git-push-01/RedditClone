caa74af730ccb27ee01d70d4d1c38299b5c0072c
Hugo Escandon <hescandon6@gmail.com>
2020-10-09 09:38:56 -0400

    update index.js and Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-worker: node getPosts.js
+
 web: node index.js
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
- // const module1 = require("./getPosts")
+const module1 = require("./getPosts")
 const schedule = require("node-schedule");
 const module2 = require("./postResults")
 const module3 = require("./textSend")
-// module1()
+module1()
 module2()
 const schechJob = schedule.scheduleJob("* * * * *", function () {
 module3()
